0bba72e1ac6831fe282dddc01ab6bfbe276a0059
rgb-katana <komarova1610@yandex.ru>
2023-04-20 22:36:07 +0300

    game and ship factories

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,71 @@
-import "../sass/main.scss";
+'use strict';
 
-export function sayHi() {
-  return "Hello";
+import '../sass/main.scss';
+
+export function Ship(length) {
+  return {
+    length,
+    numOfHits: 0,
+    isSunkBool: false,
+    hit: function () {
+      this.numOfHits++;
+    },
+    isSunk: function () {
+      if (this.numOfHits === this.length) {
+        this.isSunkBool = true;
+      }
+    },
+  };
 }
+
+export function Gameboard() {
+  const board = [];
+  for (let i = 0; i < 10; i++) {
+    const row = [];
+    for (let i = 0; i < 10; i++) {
+      row.push(0);
+    }
+    board.push(row);
+  }
+
+  return {
+    board,
+    placeShip: function (beginCoordinate, endCoordinate, length) {
+      if (beginCoordinate[0] === endCoordinate[0]) {
+        const constantCoord = beginCoordinate[0];
+        let changingCoord = beginCoordinate[1];
+        for (let i = 0; i < length; i++) {
+          this.board[constantCoord][changingCoord++] = length;
+        }
+      } else if (beginCoordinate[1] === endCoordinate[1]) {
+        const constantCoord = beginCoordinate[1];
+        let changingCoord = beginCoordinate[0];
+        for (let i = 0; i < length; i++) {
+          this.board[changingCoord++][constantCoord] = length;
+        }
+      }
+    },
+    printBoard: function () {
+      console.log('NEW BOARD');
+      this.board.map(row => {
+        console.log(row.join(' '));
+        console.log('-');
+      });
+    },
+    recieveAttack: function (attackCoordinate) {
+      if (this.board[attackCoordinate[0]][attackCoordinate[1]] === 0) {
+        this.board[attackCoordinate[0]][attackCoordinate[1]] = 'M';
+      } else if (this.board[attackCoordinate[0]][attackCoordinate[1]] !== 0) {
+        this.board[attackCoordinate[0]][attackCoordinate[1]] = 'H';
+      }
+    },
+  };
+}
+
+const game = Gameboard();
+game.printBoard();
+game.placeShip([0, 0], [0, 2], 2);
+game.placeShip([1, 0], [6, 0], 5);
+game.placeShip([5, 9], [9, 9], 5);
+game.placeShip([2, 4], [7, 4], 5);
+game.printBoard();
--- src/js/controller.test.js
+++ src/js/controller.test.js
@@ -1,5 +1,33 @@
-import { sayHi } from "./controller";
+import { Ship } from './controller';
 
-test("sample test 1", () => {
-  expect(sayHi()).toBe("Hello");
+test('Hitting 1 time', () => {
+  const carrier = Ship(5);
+  if (carrier.numOfHits === 0) {
+    carrier.hit();
+    expect(carrier.numOfHits).toBe(1);
+  }
+});
+
+test('Sinking the ship with length 5', () => {
+  const carrier = Ship(5);
+  for (let i = 0; i < 4; i++) {
+    carrier.hit();
+    carrier.isSunk();
+    expect(carrier.isSunkBool).toBe(false);
+  }
+  carrier.hit();
+  carrier.isSunk();
+  expect(carrier.isSunkBool).toBe(true);
+});
+
+test('Sinking the ship with length 3', () => {
+  const carrier = Ship(3);
+  for (let i = 0; i < 2; i++) {
+    carrier.hit();
+    carrier.isSunk();
+    expect(carrier.isSunkBool).toBe(false);
+  }
+  carrier.hit();
+  carrier.isSunk();
+  expect(carrier.isSunkBool).toBe(true);
 });
